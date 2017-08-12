b9893e06dafa824ed4ff0d31e991e54ee1d6766e
周立海 <734744695@qq.com>
2017-08-12 10:52:51 +0800

    改进后台

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,18 +1,14 @@
 @extends('../layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                @include('layouts.message')
-            <form action="{{ route('category.store') }}" method="POST">
-                {{ csrf_field() }}
-                
-                <input type="hidden" name="id" value="{{ isset($id) ? $id : old('id') }}">
-                <input class="form-control" rows="1" placeholder="名称" name="name" value="{{ isset($name) ? $name : old('name') }}">
-                <button type="submit" class="btn btn-primary pull-right">发布</button>
-            </form>
-            </div>
-        </div>
+    @include('layouts.message')
+    <div class="center-block">
+        <form action="{{ route('category.store') }}" method="POST">
+            {{ csrf_field() }}
+
+            <input type="hidden" name="id" value="{{ isset($id) ? $id : old('id') }}">
+            分类名称:<input name="name" value="{{ isset($name) ? $name : old('name') }}">
+            <button type="submit">发布</button>
+        </form>
     </div>
 @endsection
--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -1,29 +1,17 @@
 @extends('../layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <table class="table">
-                    <caption>标签列表</caption>
-                    <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>名称</th>
-                        <th>操作</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($tags as $tag)
-                        <tr>
-                            <td>{{ $tag['id'] }}</td>
-                            <td><a href="{{ route('tag.update',  ['id' => $tag['id']]) }}">{{ $tag['name'] }}</a></td>
-                            <td><a class="btn btn-danger" href="{{ route('tag.destroy', ['id' => $tag['id']]) }}">删除</a></td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
+    <div class="center-block">
+        <ol>
+            @foreach ($tags as $tag)
+                <li>
+                    <span>{{ $tag['id'] }}</span>
+
+                    <a href="{{ route('tag.update',  ['id' => $tag['id']]) }}">{{ $tag['name'] }}</a>
+                    ->
+                    <a href="{{ route('tag.destroy', ['id' => $tag['id']]) }}">删除</a>
+                </li>
+            @endforeach
+        </ol>
     </div>
 @endsection
