e8009c32b632da86fe0a386cbde7d2ad9e07a6ae
vagrant <734744695@qq.com>
2017-08-14 12:28:51 +0000

    修改时间显示

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="article-header">
         <a href="{{ route('article.show', $article->id )}}">{{ $article->title }}</a>
-        <p class="article-sub-header"> {{ $article->category['name'] }} {{ $article->created_at }} </p>
+        <p class="article-sub-header"> {{ $article->category['name'] }} {{ $article->created_at->toFormattedDateString() }} </p>
     </div>
     <div class="article-content">
         {!! $article['content'] !!}
