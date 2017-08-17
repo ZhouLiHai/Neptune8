447d95c0e84288e26e9917406d7133418dc6a313
周立海 <734744695@qq.com>
2017-08-17 20:43:30 +0800

    改变网站口号和布局

--- resources/views/layouts/sub-head.blade.php
+++ resources/views/layouts/sub-head.blade.php
@@ -2,5 +2,5 @@
     {{--<img src="{{ URL::asset("/images/front-page.jpg") }}">--}}
 {{--</div>--}}
 <div class="slogan">
-    “她还太年轻，不知道生活并不会免费地赠与，而人自命运中收受的一切，早已在暗中标好了价格。”
+    Practice make perfect.
 </div>
